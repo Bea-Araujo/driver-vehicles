66df89d391c14961f9207705443324d5e1413bd4
beatriz abreu <beatriz.abreu@nhs.com.br>
2024-02-20 15:13:42 -0300

    chore: aplica estilo a header

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@mui/material";
 import style from './header.module.css'
 import Image from "next/image";
+import Link from "next/link";
+
+import gobraxLogo from '@/../public/gobraxLogo.png'
 
 export default function Header(){
     return (
         <header className={style.headerContainer}>
             <div className={style.navigationContainer}>
+            <Link href="/drivers">
                 <Button size="medium">Motoristas</Button>
+            </Link>
+            <Link href="/vehicles">
                 <Button size="medium">Veículos</Button>
+            </Link>
             </div>
 
-            <Image src="" alt="gobrax logo"/>
-            <Button variant="contained" size="medium">LinkedIn</Button>
+            <Image src={gobraxLogo} width={100} alt="gobrax logo"/>
+            <Link href="https://www.linkedin.com/in/beatriz-araujo-dev/">
+               <Button variant="contained" size="medium">LinkedIn</Button>
+            </Link>
         </header>
     )
 }
