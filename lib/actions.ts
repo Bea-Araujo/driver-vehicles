957d27d2af563bded77c4f4fe09a22e250883990
beatriz abreu <beatriz.abreu@nhs.com.br>
2024-02-20 22:36:13 -0300

    chore: adiciona actions para vehicles

--- lib/actions.ts
+++ lib/actions.ts
@@ -22,9 +22,7 @@ export const createDriver = async (formData: FormData) => {
 
 export const updateDriver = async (formData: FormData, id:string) => {
     try {
-        console.log(Object.fromEntries(formData))
         const { name, document, vehicleId } = Object.fromEntries(formData)
-        console.log(vehicleId)
         const updatedDriverData = {
             id,
             name,
@@ -47,19 +45,69 @@ export const updateDriver = async (formData: FormData, id:string) => {
 }
 
 export const deleteDriver = async (driverId: string) => {
-
+    try {
+        const response = await fetch(`http://localhost:3000/api/drivers/${driverId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        throw new Error('Failed to delete driver')
+    }
 }
 
 export const createVehicle = async (formData: FormData) => {
-
+    try {
+        const { carPlate, brand } = Object.fromEntries(formData)
+        const newVehicle = {
+            carPlate,
+            brand,
+        }
+    
+        const response = await fetch('http://localhost:8000/vehicle', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newVehicle)
+        })
+    } catch(e) {
+        throw new Error('Failed to create vehicle')
+    }
 }
 
-export const updateVehicle = async (formData: FormData) => {
+export const updateVehicle = async (formData: FormData, vehicleId: string) => {
+    try {
+        const { carPlate, brand } = Object.fromEntries(formData)
+        const updatedVehicleData = {
+            carPlate,
+            brand,
+        }
 
+        const response = await fetch(`http://localhost:8000/vehicle/${vehicleId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedVehicleData)
+        })
+    } catch(e) {
+        throw new Error('Failed to create vehicle')
+    }
 }
 
 export const deleteVehicle = async (vehicleId: string) => {
-
+    try {
+        const response = await fetch(`http://localhost:8000/vehicle/${vehicleId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        throw new Error('Failed to delete vehicle')
+    }
 }
 
 
--- lib/fetchDrivers.ts
+++
@@ -1,9 +0,0 @@
-
-export async function getDriversData(){
-    try {
-        const res = await fetch('http://localhost:8000/drivers')
-        return res.json()
-    } catch (error) {
-        throw new Error("Failed to fetch drivers")
-    }
-}
